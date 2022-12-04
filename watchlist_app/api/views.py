d064f91701689797b7d07e8725e7d658968b5ece
Shahjamal Biswas <shahjamal570@gmail.com>
2022-12-04 15:23:13 +0530

    Field level, object level validations, core validators

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,7 +1,7 @@
 from watchlist_app.models  import Movie
 from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+#from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
 
